1b7ab9cfec7f0e64af1edc89a1d709084b6ee9ab
Olaoluwa Mustapha <jomeemustapha@gmail.com>
2021-01-09 09:38:45 -0600

    Added BaseButton component for better button microinteraction

--- src/components/general-components/general.js
+++ src/components/general-components/general.js
@@ -1,6 +1,8 @@
-import React from 'react';
 import styled from 'styled-components';
 
-import { useLocation } from 'react-router-dom';
 import { m as motion } from 'framer-motion';
-import { defaultPageTransitionVariants } from '../utils/framer-variants';
+
+export const BaseButton = styled(motion.button).attrs({
+  layout: 'position',
+  whileTap: { scale: 0.9 },
+})``;
